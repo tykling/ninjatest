636578850cc483deaeb90545d41e755256d92d9f
Thomas Steen Rasmussen <tykling@bornhack.org>
2022-12-22 12:17:10 +0100

    some code

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ninjatest
+Small testproject to help debug https://github.com/vitalik/django-ninja/issues/610
---
+++ src/ninjatest/api.py
@@ -0,0 +1,30 @@
+from ninja import NinjaAPI, Schema, ModelSchema
+from django.http import HttpResponse
+from ninja.operation import ResponseObject
+from django.contrib.auth.models import User
+
+class UserOut(Schema):
+    """The schema with two str fields where one has a resolve_ method."""
+    username: str
+    foo: str
+
+    def resolve_foo(self, obj):
+        return "resolved foo"
+
+api = NinjaAPI()
+
+@api.get("/schema/returnmodel-works", response=UserOut)
+def returnmodel_works(request):
+    """Return a model instance directly, the foo field is resolved."""
+    return User.objects.get_or_create(username="test")[0]
+
+@api.get("/schema/returndict-broken", response=UserOut)
+def returndict_broken(request):
+    """Return a dict directly, the foo field is not resolved."""
+    return {"username": "some user"}
+
+@api.get("/schema/returndict-manual-works", response=UserOut)
+def returndict_works(request):
+    """Use the UserOut schema manually, the foo field is resolved."""
+    schema = UserOut.from_orm({"username": "some user"})
+    return HttpResponse(schema.json())
